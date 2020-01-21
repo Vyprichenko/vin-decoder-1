b8bbbac85a1e3021456989b5f5ddee74ad604c15
tarthur <terzi.arthur@gmail.com>
2020-01-21 15:51:01 +0200

    replacing BrowserRouter with HashRouter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../../containers/home';
 import Layout from '../layout';
 import Variables from '../variables';
